dca2cb9be0cc842d4f3f42117558c58db83549ef
Maxi_Lopez <m.lopez@itecriocuarto.org.ar>
2025-08-15 19:59:41 -0300

    Prueba: mensaje para verificar deploy

--- math.test.js
+++ math.test.js
@@ -3,5 +3,5 @@ const { suma, resta, multiplicar } = require('./math');
 console.assert(suma(2, 3) === 5, 'Error en suma');
 console.assert(resta(5, 3) === 2, 'Error en resta');
 console.assert(multiplicar(4, 2) === 8, 'Error en multiplicar');
-
+console.log("Prueba deploy");
 console.log("✅ Todas las pruebas pasaron correctamente");
