877ae89f31ba1c0cb44c1ec920c282d2e6e4d5fb
Maxi_Lopez <m.lopez@itecriocuarto.org.ar>
2025-08-18 22:53:15 -0300

    Para deploy

--- math.test.js
+++ math.test.js
@@ -4,6 +4,7 @@ console.assert(suma(2, 3) === 5, 'Error en suma');
 console.assert(resta(5, 3) === 2, 'Error en resta');
 console.assert(suma(1,1) === 2, 'Error en sumar' );
 console.assert(multiplicar(4, 2) === 8, 'Error en multiplicar');
+console.assert(multiplicar(6, 2) === 12, 'Error en multiplicar');
 
 console.assert(dividir(10, 0) === "Error: división por cero", 'Error en división por cero');
 
